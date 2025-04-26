00717f9db9540dff51627833a0d2e0490ccb5933
Jakelin <jakita8889@gmail.com>
2025-04-26 00:09:20 -0600

    Primer commit - Proyecto Android creado

---
+++ app/src/test/java/com/example/proyectopdm2025_gt03_grupo5_gestion_de_gastos_personales_mensuales/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.proyectopdm2025_gt03_grupo5_gestion_de_gastos_personales_mensuales;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ProyectoPDM2025-GT03-Grupo5-Gestion_de_Gastos_Personales_Mensuales"
+include(":app")
