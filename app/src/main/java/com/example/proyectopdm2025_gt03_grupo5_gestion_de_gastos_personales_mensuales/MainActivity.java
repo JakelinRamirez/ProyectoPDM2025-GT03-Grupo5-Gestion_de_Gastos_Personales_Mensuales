ea65507c7687ef5b0e7b952b89cd78c353a6e8a1
Jakelin <jakita8889@gmail.com>
2025-04-26 22:05:04 -0600

    pantalla de analisis echa

--- app/src/main/java/com/example/proyectopdm2025_gt03_grupo5_gestion_de_gastos_personales_mensuales/MainActivity.java
+++ app/src/main/java/com/example/proyectopdm2025_gt03_grupo5_gestion_de_gastos_personales_mensuales/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.proyectopdm2025_gt03_grupo5_gestion_de_gastos_personales_mensuales;
-
+import com.example.proyectopdm2025_gt03_grupo5_gestion_de_gastos_personales_mensuales.ui.analysis.AnalysisActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.content.Intent;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Spinner comboCategoria;
+    Spinner comboCategoria2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Crear un Intent para abrir AnalysisActivity automáticamente
+        Intent intent = new Intent(MainActivity.this, AnalysisActivity.class);
+        startActivity(intent);  // Iniciar AnalysisActivity
+
+        // Opcional: Finalizar MainActivity si no quieres que el usuario regrese a esta actividad
+        finish();
     }
 }
--- app/src/main/java/com/example/proyectopdm2025_gt03_grupo5_gestion_de_gastos_personales_mensuales/ui/analysis/AnalysisActivity.java
+++ app/src/main/java/com/example/proyectopdm2025_gt03_grupo5_gestion_de_gastos_personales_mensuales/ui/analysis/AnalysisActivity.java
@@ -1,4 +1,37 @@
 package com.example.proyectopdm2025_gt03_grupo5_gestion_de_gastos_personales_mensuales.ui.analysis;
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 
-public class AnalysisActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.proyectopdm2025_gt03_grupo5_gestion_de_gastos_personales_mensuales.R;
+
+public class AnalysisActivity extends AppCompatActivity{
+    Spinner comboCategoria, comboCategoria2, comboMes1,comboMes2;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_analysis);
+        //Arreglo del spinner 1
+        comboCategoria=(Spinner) findViewById(R.id.spinnerCategorias);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Categorias, android.R.layout.simple_spinner_item);
+        comboCategoria.setAdapter(adapter);
+        //Arreglo del spinner 2
+        comboCategoria2=(Spinner) findViewById(R.id.spinnerPeriodo);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.Periodos, android.R.layout.simple_spinner_dropdown_item);
+        comboCategoria2.setAdapter(adapter2);
+
+        //spinner de mes
+        comboMes1=(Spinner) findViewById(R.id.spinnerMes1);
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this, R.array.Mes, android.R.layout.simple_spinner_dropdown_item);
+        comboMes1.setAdapter(adapter3);
+
+        comboMes2=(Spinner) findViewById(R.id.spinnerMes2);
+        ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(this, R.array.Mes, android.R.layout.simple_spinner_dropdown_item);
+        comboMes2.setAdapter(adapter4);
+    }
 }
